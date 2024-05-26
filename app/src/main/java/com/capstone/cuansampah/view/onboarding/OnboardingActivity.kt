7c545f43586bf4ee40bf1e5f203dff8583b4fb0c
hiqmalism <hernandanda30@gmail.com>
2024-05-26 21:22:13 +0700

    Make the capstone project - Cuan Sampah

---
+++ app/src/main/java/com/capstone/cuansampah/view/onboarding/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.cuansampah.view.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.cuansampah.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
