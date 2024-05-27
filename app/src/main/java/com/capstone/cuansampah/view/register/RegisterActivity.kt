d285727679b6f719455ec537dcb28ee8658ef588
Flye <212103019@ftti.unjaya.ac.id>
2024-05-27 07:19:10 +0700

    add Register

--- app/src/main/java/com/capstone/cuansampah/view/register/RegisterActivity.kt
+++ app/src/main/java/com/capstone/cuansampah/view/register/RegisterActivity.kt
@@ -1,12 +1,42 @@
 package com.capstone.cuansampah.view.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.capstone.cuansampah.R
+import com.capstone.cuansampah.databinding.ActivityRegisterBinding
+import com.capstone.cuansampah.view.login.LoginActivity
+
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupView()
+        setupAction()
+    }
+
+    private fun setupView() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setBackgroundDrawable(null)
+        supportActionBar?.title = ""
+    }
+
+    private fun setupAction() {
+        binding.btnRegister.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+
+        binding.tvLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
