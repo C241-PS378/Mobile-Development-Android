eac490c6475bed338594bba3743a8821278e087c
Flye <212103019@ftti.unjaya.ac.id>
2024-05-29 09:20:19 +0700

    .

---
+++ app/src/main/java/com/capstone/cuansampah/view/login/ResetPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.cuansampah.view.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.cuansampah.R
+
+class ResetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+    }
+}
